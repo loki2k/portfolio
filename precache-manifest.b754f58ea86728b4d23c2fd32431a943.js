7eab4c69d70419075e01254a1d9b9bc162184988
loki2k <jeremylagarto@gmail.com>
2020-12-13 13:19:41 -0800

    Updates

---
+++ precache-manifest.b754f58ea86728b4d23c2fd32431a943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f364e05b263875bad713670e3ec881b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c04e194188d3570c7fb",
+    "url": "/portfolio/static/css/main.cb455c11.chunk.css"
+  },
+  {
+    "revision": "d90e85501adf1bcc0842",
+    "url": "/portfolio/static/js/2.9835ac7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9835ac7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c04e194188d3570c7fb",
+    "url": "/portfolio/static/js/main.8b8ed14d.chunk.js"
+  },
+  {
+    "revision": "33e3eb3d60fc778ec147",
+    "url": "/portfolio/static/js/runtime-main.d2c2cec5.js"
+  },
+  {
+    "revision": "da5ea95451ff788260c135bae452ca69",
+    "url": "/portfolio/static/media/profile.da5ea954.jpg"
+  }
+]);
